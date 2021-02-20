3420f508bde4e1cbb0d73efc294c2f897169bc4b
Tor Smedberg <tor.smedberg@gmail.com>
2021-02-20 12:06:53 +0100

    get port from env variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const https = require('https');
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT | 8080; //get port number from env variable, used by Heroku, otherwise fall back onto port 8080
 const corsAllow = '*';
 
 app.use(express.json())
